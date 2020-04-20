b9ea8cd3418145ef9698266a60c05c32f8cd7ed1
Daniel Guzman <daniel.e.guzman.s@gmail.com>
2020-04-19 23:33:01 -0400

    delete file serve

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/src/app/app.component.html'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/src/app/app.component.html'));});
-app.listen(process.env.PORT || 8080);
